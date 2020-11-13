88908b6241b88992a5595b07250f87d6639e7087
Gabrielli Venerando <57273257+gabriellivenerando@users.noreply.github.com>
2020-11-13 13:38:36 -0300

    Add files via upload

---
+++ poss-V2-main-novo/poss-V2-main/script.js
@@ -0,0 +1,14 @@
+$("document").ready(function(){
+    $(".navbar__ham").on("click", function(){
+      $(".navbar__links").toggleClass("active");
+      
+    });
+  
+    $(".navbar__links a li").on("click", function(){
+      $(".navbar__links").removeClass("active");
+    });
+  });
+  
+  
+  
+  
